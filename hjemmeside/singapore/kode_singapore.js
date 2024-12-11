0623c06c801ef5456593e5f0d536c40d03898e8f
philipp2022t <carl7699@rysensteen.dk>
2024-12-11 10:54:10 +0100

    Add files via upload

--- hjemmeside/singapore/kode_singapore.js
+++ hjemmeside/singapore/kode_singapore.js
@@ -2,10 +2,10 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos -0.5) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
 }
